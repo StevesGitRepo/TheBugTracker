08c3e0e2d2bb1ca5240a0a8b1eb93d6e12623936
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-03-28 23:13:54 -0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Data
+{
+    //ApplicationDbContext extends IdentityDbContext and supplied IdentityDbContex with type parameter of BTUser
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/BTUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    //BTUser extends from IdentityUser data
+    public class BTUser : IdentityUser
+    {
+
+    //We extended by adding FirstName, LastName, FullName
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        //string interpolation, concatination
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
