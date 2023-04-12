e0def36dc3ae5c90c671bd7411ec1df12e5b799a
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-04-12 08:08:08 -0700

    Services folder/Interfaces created

---
+++ Services/BTRolesService.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    //Class contains implementation code
+    public class BTRolesService : IBTRolesService
+    {
+        //Dependency Injection
+
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BTUser> _userManager;
+        public BTRolesService(ApplicationDbContext context,
+                                RoleManager<IdentityRole> roleManager,
+                                UserManager<BTUser> userManager
+                                ) 
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        //add async to crate asynchronous methods
+        public async Task<bool> AddUserToRoleAsync(BTUser user, string rolename)
+        {
+            bool result = (await _userManager.AddToRoleAsync(user, rolename)).Succeeded;
+            return result;
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRoleAsync(BTUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string rolename, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> GetUsersNotInRolseAsync(string rolename, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(BTUser user, string rolename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string rolename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,27 @@
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    //There is no code but the definition of the methods or properties in an interface
+    //Classes contain implementation code
+    //everything within an Interface is public, Interfaces contain methods (functions) for classes
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string rolename);
+
+        public Task<IEnumerable<string>> GetUserRoleAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string rolename);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string rolename);
+
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string rolename, int companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRolseAsync(string rolename, int companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    
+    }
+}
