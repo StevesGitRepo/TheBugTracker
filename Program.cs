058687b273a20c21f16f0f51619236f2af884adc
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-04-23 19:55:36 -0700

    BTTicketHistoryService class added

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddScoped<IBTRolesService, BTRolesService>();
 builder.Services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
 builder.Services.AddScoped<IBTProjectService, BTProjectService>();
 builder.Services.AddScoped<IBTTicketService, BTTicketService>();
+builder.Services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
 
 builder.Services.AddControllersWithViews();
 
