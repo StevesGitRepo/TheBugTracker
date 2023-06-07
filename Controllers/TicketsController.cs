e65faef349a5ec2e6f1aa60739163e0f6c72a3af
StevesGitRepo <steve.k.szymanski@gmail.com>
2023-06-07 13:04:25 -0700

    Dashboard MVC created, localhost data reverted to original

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -208,10 +208,10 @@ namespace TheBugTracker.Controllers
         // POST: Tickets/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
 
         //these items bind to the Ticket:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,Created,TicketTypeId,TicketPriorityId")] Ticket ticket)
         {
             BTUser btUser = await _userManager.GetUserAsync(User);
